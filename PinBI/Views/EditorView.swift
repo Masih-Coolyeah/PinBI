feefbcd44f6efaa8f03cb4de38e14044d248c71d
Sry Deviani Tambunan <89495018+srydevianitambunan@users.noreply.github.com>
2024-08-07 14:38:10 +0700

    solving selectedTag

--- PinBI/Views/EditorView.swift
+++ PinBI/Views/EditorView.swift
@@ -10,30 +10,31 @@ import SwiftData
 
 struct EditorView: View {
     @Environment(\.modelContext) private var context
-    @State private var selectedTag: String? = "Dokumen Baru"
+    @State private var selectedTag: Document? // Update to use Document type
     @State var title: String = "Dokumen Baru"
     @State var content: String = "Ini adalah isi dari dokumen baru"
     
     @Query var documents: [Document]
     
-    @State private var selectedDocument: Document?
-
     var body: some View {
         NavigationSplitView {
             List(selection: $selectedTag) {
                 ForEach(documents) { document in
                     Text(document.title)
-                        .tag(document as Document?)
+                        .tag(document)
                         .foregroundColor(.gray)
-                        .contextMenu() {
+                        .contextMenu {
                             Button("Delete") {
                                 context.delete(document)
+                                if selectedTag == document {
+                                    selectedTag = nil
+                                }
                             }
                         }
                 }
             }
         } detail: {
-            if let selectedDocument = selectedDocument {
+            if let selectedDocument = selectedTag {
                 EditorDetailView(document: selectedDocument)
             } else {
                 Text("No document selected")
@@ -48,13 +49,12 @@ struct EditorView: View {
                 }
             }
         }
-
     }
     
     private func addNewDocument() {
-        let newDocument = Document(title: "\(title) \(documents.count+1)", date: Date(), mistakeList: [], correctText: "", listText: [])
+        let newDocument = Document(title: "\(title) \(documents.count + 1)", date: Date(), mistakeList: [], correctText: "", listText: [])
         context.insert(newDocument)
-        selectedDocument = newDocument
+        selectedTag = newDocument // Update to set the newly created document as selected
     }
 }
 
